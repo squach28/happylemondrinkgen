da077d71f7c2c0c11d0db98037f526bde8ef0bf2
squach28 <squach3443@gmail.com>
2020-07-17 11:48:00 -0700

    added console log

--- finder/googleapiscript.js
+++ finder/googleapiscript.js
@@ -1,6 +1,8 @@
 // Create the script tag, set the appropriate attributes
+console.log(process.env.API_KEY);
 var script = document.createElement('script');
 script.src = 'https://maps.googleapis.com/maps/api/js?key=" + process.env.API_KEY + "&callback=initMap';
+
 script.defer = true;
 script.async = true;
 
