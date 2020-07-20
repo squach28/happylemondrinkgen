964c26ec5aa6c11a12153ffcfc80f7e8dbe74a50
squach28 <49498077+squach28@users.noreply.github.com>
2020-07-20 00:24:59 -0700

    Update create-env.js

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,6 +1,17 @@
 const fs = require('fs');
+const http = require('https');
 fs.writeFileSync('./.env', `API_KEY=${process.env.API_KEY}\n`);
 
 const env = require('dotenv').config();
 console.log(process.env.API_KEY);
+
+
+https.get(https://maps.googleapis.com/maps/api/js?key=" + process.env.API_KEY + "&callback=initMap", (res) => {
+  console.log('statusCode:', res.statusCode);
+  console.log('headers:', res.headers);
+
+  res.on('data', (d) => {
+    process.stdout.write(d);
+  });
+
 
