ed855cfbd74a78173bc5b7e2c1778cdce1543277
squach28 <49498077+squach28@users.noreply.github.com>
2020-07-20 00:20:45 -0700

    Update create-env.js

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,3 +1,6 @@
-const fs = require('fs')
-fs.writeFileSync('./.env', `API_KEY=${process.env.API_KEY}\n`)
+const fs = require('fs');
+fs.writeFileSync('./.env', `API_KEY=${process.env.API_KEY}\n`);
+
+const env = require('dotenv').config();
+console.log(process.env.API_KEY);
 
