6d30babce738490c4397608a737ba6acd9e19de3
squach28 <49498077+squach28@users.noreply.github.com>
2020-07-20 11:06:01 -0700

    env script build env variables

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -4,18 +4,22 @@ fs.writeFileSync('./.env', `API_KEY=${process.env.API_KEY}\n`);
 
 const env = require('dotenv').config();
 console.log(process.env.API_KEY);
+var loadMapString = `
+// Create the script tag, set the appropriate attributes
+var script = document.createElement('script');
+script.src = 'https://maps.googleapis.com/maps/api/js?key=` + process.env.API_KEY + `&callback=initMap';
+script.defer = true;
+script.async = true;
 
+// Attach your callback function to the window object
+window.initMap = function() {
+  // JS API is loaded and available
+};
 
-https.get("https://maps.googleapis.com/maps/api/js?key=" + process.env.API_KEY + "&callback=initMap", (res) => {
-  console.log('statusCode:', res.statusCode);
-  console.log('headers:', res.headers);
+// Append the script element to head
+document.head.appendChild(script); `;
 
-  res.on('data', (d) => {
-    process.stdout.write(d);
-  });
-  
-  }).on('error', (e) => {
-  console.error(e);
+fs.writeFile("scripts/../finder/loadmap.js", loadMapString, function(err) {
+    if(err) throw err;
+    console.log('file created!');
 });
-
-
