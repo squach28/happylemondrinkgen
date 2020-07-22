3cbdf8c81116237b18362fcc3e3aeb0dcd09c3f2
squach28 <squach3443@gmail.com>
2020-07-22 00:52:20 -0700

    initmap added

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -5,6 +5,15 @@ fs.writeFileSync('./.env', `API_KEY=${process.env.API_KEY}\n`);
 const env = require('dotenv').config();
 console.log(process.env.API_KEY);
 
+const finderScript = `var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.309083, lng: -121.813027 },
+    zoom: 18
+  });
+}`
+
+
 https.get("https://maps.googleapis.com/maps/api/js?key=" + process.env.API_KEY + "&callback=initMap", (res) => {
   console.log('statusCode:', res.statusCode);
   console.log('headers:', res.headers);
@@ -12,6 +21,7 @@ https.get("https://maps.googleapis.com/maps/api/js?key=" + process.env.API_KEY +
   res.on('data', (d) => {
     process.stdout.write(d);
     fs.appendFileSync('./finder/googlemapsapi.js', d);
+    fs.appendFileSync('./finder/googlemapsapi.js', finderScript)
   });
 
   }).on('error', (e) => {
