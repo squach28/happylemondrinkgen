709b88c69cceb0e7e799f280c842a0e278099dae
squach28 <squach3443@gmail.com>
2020-07-22 00:17:32 -0700

    fixed quotes

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -4,7 +4,7 @@ fs.writeFileSync('./.env', `API_KEY=${process.env.API_KEY}\n`);
 
 const env = require('dotenv').config();
 console.log(process.env.API_KEY);
-console.log(https.get('https://maps.googleapis.com/maps/api/js?key=` + process.env.API_KEY + `&callback=initMap'));
+console.log(https.get('https://maps.googleapis.com/maps/api/js?key=' + process.env.API_KEY + '&callback=initMap'));
 
 
 
