fadcbf45d1560ce4a645f60a513b41b5c81df880
squach28 <squach3443@gmail.com>
2020-07-22 01:12:31 -0700

    timeout for finderscript

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -5,6 +5,8 @@ fs.writeFileSync('./.env', `API_KEY=${process.env.API_KEY}\n`);
 const env = require('dotenv').config();
 console.log(process.env.API_KEY);
 
+
+
 const finderScript = `var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -12,6 +14,10 @@ function initMap() {
     zoom: 18
   });
 }`
+
+function appendFinderScript() {
+  fs.appendFileSync('./finder.googlemapsapi.js', finderScript);
+}
 
 
 https.get("https://maps.googleapis.com/maps/api/js?key=" + process.env.API_KEY + "&callback=initMap", (res) => {
@@ -21,12 +27,14 @@ https.get("https://maps.googleapis.com/maps/api/js?key=" + process.env.API_KEY +
   res.on('data', (d) => {
     process.stdout.write(d);
     fs.appendFileSync('./finder/googlemapsapi.js', d);
-    fs.appendFileSync('./finder/googlemapsapi.js', finderScript);
   });
 
   }).on('error', (e) => {
   console.error(e);
   });
+
+  setTimeout(appendFinderScript(), 3000);
+
 
 
   
