66c21abf5f7c6fe0dbc72ac2e3b5aec7e7d482d0
squach28 <squach3443@gmail.com>
2020-07-22 01:05:39 -0700

    put back sync

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -21,7 +21,7 @@ https.get("https://maps.googleapis.com/maps/api/js?key=" + process.env.API_KEY +
   res.on('data', (d) => {
     process.stdout.write(d);
     fs.appendFileSync('./finder/googlemapsapi.js', d);
-    fs.appendFile('./finder/googlemapsapi.js', finderScript);
+    fs.appendFileSync('./finder/googlemapsapi.js', finderScript);
   });
 
   }).on('error', (e) => {
