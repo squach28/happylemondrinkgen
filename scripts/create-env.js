b046319b7798f4c3db0f9a11c22384f0e4544647
squach28 <squach3443@gmail.com>
2020-07-22 00:13:20 -0700

    testing api key

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -4,20 +4,7 @@ fs.writeFileSync('./.env', `API_KEY=${process.env.API_KEY}\n`);
 
 const env = require('dotenv').config();
 console.log(process.env.API_KEY);
-var loadMapString = `
-// Create the script tag, set the appropriate attributes
-var script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=` + process.env.API_KEY + `&callback=initMap';
-script.defer = true;
-script.async = true;
-
-// Attach your callback function to the window object
-window.initMap = function() {
-  // JS API is loaded and available
-};
-
-// Append the script element to head
-document.head.appendChild(script); `;
+console.log(https.get('https://maps.googleapis.com/maps/api/js?key=` + process.env.API_KEY + `&callback=initMap'));
 
 
 
