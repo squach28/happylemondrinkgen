69ba134c9ea2976a6aaf2c25c6e31da28d53b5ce
squach28 <49498077+squach28@users.noreply.github.com>
2020-07-22 23:05:51 -0700

    yelp api added, google maps api removed

--- scripts/create-env.js
+++ scripts/create-env.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const https = require('https');
+const yelp = require('yelp-fusion');
 fs.writeFileSync('./.env', `API_KEY=${process.env.API_KEY}\n`);
-
+fs.appendFileSync('./.env', `YELP_API_KEY=${process.env.YELP_API_KEY}\n`);
+/*
 const finderScript = `var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
@@ -12,13 +14,14 @@ function initMap() {
 
 function appendFinderScript() {
   fs.appendFileSync('./finder/googlemapsapi.js', finderScript);
-}
+} */
 
 const env = require('dotenv').config();
 console.log(process.env.API_KEY);
+console.log(process.env.YELP_API_KEY);
 //appendFinderScript();
 
-
+/*
 https.get("https://maps.googleapis.com/maps/api/js?key=" + process.env.API_KEY + "&callback=initMap", (res) => {
   console.log('statusCode:', res.statusCode);
   console.log('headers:', res.headers);
@@ -30,8 +33,26 @@ https.get("https://maps.googleapis.com/maps/api/js?key=" + process.env.API_KEY +
 
   }).on('error', (e) => {
   console.error(e);
-  });
+  }); */
 
+
+  const searchRequest = {
+    term:'Happy Lemon',
+    location: 'san jose, ca'
+  };
+  
+  
+  const client = yelp.client(process.env.YELP_API_KEY);
+  
+  
+  client.search(searchRequest).then(response => {
+    const firstResult = response.jsonBody.businesses;
+    const prettyJson = JSON.stringify(firstResult, null, 4);
+    console.log(prettyJson);
+    console.log(response.jsonBody.total);
+  }).catch(e => {
+    console.log(e);
+  });
   
 
 
